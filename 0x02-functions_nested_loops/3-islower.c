21114934b938267000551eae0aebb1301e7f175e
myusername <myemail@gmail.com>
2023-06-21 12:12:44 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,16 +7,13 @@
  * alphabet in lowercase 10 times
  * Return: 1 if is lowercase or 0 if is uppercase
  */
+
 int _islower(int c)
+
 {
-	char i;
-	int lower = 0;
-
-	for (i = 'a'; i <= 'z'; i++)
+	if (c >= 97 && c <= 122)
 	{
-		if (i == c)
-			lower = 1;
+		return (1);
 	}
-
-	return (lower);
+	return (0);
 }
