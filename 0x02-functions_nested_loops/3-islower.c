02ad20dcd6ca44d9161b56ae0d80509ba5e759b7
myusername <myemail@gmail.com>
2023-06-21 13:16:07 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _islower - Check Johnel
- * @c: An input character
- * Description: function uses _putchar function to print
- * alphabet in lowercase 10 times
- * Return: 1 if is lowercase or 0 if is uppercase
+ * _islower - check if the given character is lower case.
+ * @c: character to test.
+ *
+ * Return: 1 (True) else 0 (False)
  */
-
 int _islower(int c)
-
 {
 	if (c >= 97 && c <= 122)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
+
