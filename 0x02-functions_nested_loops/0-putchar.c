fb03b8a4a62a6c3ad26c3dddee9e4b29560e8897
myusername <myemail@gmail.com>
2023-06-21 11:47:20 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,22 @@
 #include "main.h"
-	/**
-	 * main - print _putchar
-	 * is written by Rolivhuwa
-	 * Return: 0
-	 */
 
-
-	int main(void)
-	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
 	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
-	return (0);
-	}
 
+	return (0);
+}
