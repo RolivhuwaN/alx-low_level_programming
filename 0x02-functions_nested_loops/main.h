57942532a6e71fdfc86519a28d2f52e5cffc3a9a
myusername <myemail@gmail.com>
2023-06-21 03:36:50 -0700

    Update

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int print_sign(int n);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+void print_alphabet_x10(void);
+void print_alphabet(void);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+#endif
