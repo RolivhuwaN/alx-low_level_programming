2282ce713451fe199400929b673562e4d0f19c39
myusername <myemail@gmail.com>
2023-07-18 11:46:55 -0700

    updated-dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef _DOG_H_
+#define _DOG_H_
 /**
- * struct dog - struture of a dog
+ * struct dog - structure containing characteristics of a dog
  * @name: name of dog
  * @age: age of dog
- * @owner: owner of dog
+ * @owner: name of owner
  */
-
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
-}
-dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+#endif /*_DOG_H_*/
